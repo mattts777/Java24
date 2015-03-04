43998f2c8de11cce808468055bb352f1b74293c7
mattts777 <matthew_brown@hotmail.com>
2015-03-04 14:57:30 -0800

    Added Operator section and additional comments

--- src/Variables.java
+++ src/Variables.java
@@ -1,5 +1,6 @@
 class Variable {
     public static void main(String[] args) {
+        //Variable Types
         int tops;
         float GradePointAverage; //holds decimal numbers up to 38 places.
         double mydouble; //holds decimal numbers up to 300 places.
@@ -10,9 +11,34 @@ class Variable {
         long USNationalDebt;//-9.22 quintillion to 9.22 quintillion
         boolean gameOver = false;//True or False
         
+        //Constants
         final int TOUCHDOWN = 6;//capitalize constants
         final int FIELDGOAL = 3;
         final int PAT = 1;
         final int TWOPTCONV = 2;
+        
+        //Operators
+        double weight = 205;
+        weight = weight + 10; //Addition
+        
+        weight = weight - 15; //Subtraction
+        
+        weight = weight / 3; //Division
+        
+        //This statement finds the remainder of 245 divided by 3:
+        int remainder = 245 % 3;
+        
+        int score = 10;
+        int total = 500 + (score * 12); //Multiplication and addition
+        
+        //Incrementing and Decrementing
+        int x = 3;
+        int answer = x++ * 10; //x++ = postfixing incrementing by 1
+        int y = 3;
+        int result = ++y * 10; //++y = prefixing incrementing by 1
+        
+        System.out.println("answer = " + answer);
+        System.out.println("result = " + result);
+        
     }
 }
