d5ba05c18c906b41d42ef3e434113f5d898c4bf8
mattts777 <matthew_brown@hotmail.com>
2015-03-04 14:19:02 -0800

    Added NFL constant declarations

--- src/Variables.java
+++ src/Variables.java
@@ -9,5 +9,10 @@ class Variable {
         short roomNumber = 777;//-32768 to 32767
         long USNationalDebt;//-9.22 quintillion to 9.22 quintillion
         boolean gameOver = false;//True or False
+        
+        final int TOUCHDOWN = 6;//capitalize constants
+        final int FIELDGOAL = 3;
+        final int PAT = 1;
+        final int TWOPTCONV = 2;
     }
 }
