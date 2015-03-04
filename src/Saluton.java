95eebf5e7acd96a3d2736b477ce6307c9b33da53
mattts777 <matthew_brown@hotmail.com>
2015-03-04 13:59:42 -0800

    Initial commit

---
+++ src/Saluton.java
@@ -0,0 +1,7 @@
+public class Saluton {
+    public static void main(String[] arguments){
+        String greeting = "Saluton mondo!";
+        System.out.println(greeting);
+        
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+class Variable {
+    public static void main(String[] args) {
+        int tops;
+        float GradePointAverage; //holds decimal numbers up to 38 places.
+        double mydouble; //holds decimal numbers up to 300 places.
+        char key = 'C';
+        String productName = "Larvets";
+        byte escapeKey = 27; //-128 to 127.
+        short roomNumber = 777;//-32768 to 32767
+        long USNationalDebt;//-9.22 quintillion to 9.22 quintillion
+        boolean gameOver = false;
+    }
+}
