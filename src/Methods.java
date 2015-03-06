a027a18b9e87ee5d34ffed8e947d03516e71e5d4
mattts777 <matthew_brown@hotmail.com>
2015-03-06 09:32:07 -0800

    Added length, case, and indexof commands

--- src/Methods.java
+++ src/Methods.java
@@ -3,6 +3,7 @@ class Methods {
     public static void main(String[] args) {
 
     //Comparing two strings
+    //NOTE: This is case sensitive
     String favorite = "Lotus Esprit";
     String guess = "Corvette Z06";
     
@@ -16,6 +17,24 @@ class Methods {
     //Changing a string's case
     String charclass = "Human";
     String changecharclass = charclass.toUpperCase();
+    
+    //Searching for a string inside a string. -1 is returned if not found.
+    //NOTE: This is case sensitive
+    
+    //Search for specific car inside a list of cars
+    String carlist = "Dodge Viper;Lotus Esprit;Chevrolet Corvette;Lamborghini Countach";
+    int stringposition = carlist.indexOf("Lotus Esprit");
+    System.out.println("We found the car at position: " + stringposition);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     }
 }
